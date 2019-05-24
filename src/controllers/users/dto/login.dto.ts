cf42d4bd775d31162304a35cbda3af8c8985c506
Ruslan Gonzalez <ruslanguns@gmail.com>
2019-05-24 18:33:04 +0100

    Swagger API Implemented in NestJS

--- src/controllers/users/dto/login.dto.ts
+++ src/controllers/users/dto/login.dto.ts
@@ -1,8 +1,14 @@
 import { IsString, IsDate, IsNotEmpty, IsBoolean, IsEmail, Length, MaxLength } from 'class-validator';
+import { ApiModelProperty } from '@nestjs/swagger';
 
 export class LoginUserDTO {
 
+    @ApiModelProperty()
     readonly username: string;
+
+    @ApiModelProperty()
     readonly email: string;
+
+    @ApiModelProperty()
     password: string;
 }
